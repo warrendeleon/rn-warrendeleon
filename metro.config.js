50e0a0ac960d3236028097b148cce0e238f9f2c3
Warren de Leon <hi@warrendeleon.com>
2022-03-01 13:07:27 +0000

    ➕ Adding SVG dependency.

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,20 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const {getDefaultConfig} = require('metro-config');
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig();
+
+  return {
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: process.env.RN_SRC_EXT
+        ? process.env.RN_SRC_EXT.split(',').concat([...sourceExts, 'svg'])
+        : [...sourceExts, 'svg'],
+    },
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+  };
+})();
