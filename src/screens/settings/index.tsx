68632cd9885e6a3337139cfbba4f8f79f124a8e9
Warren de Leon <hi@warrendeleon.com>
2022-03-04 17:53:02 +0000

    💄 Adding language selector on settings screen.

--- src/screens/settings/index.tsx
+++ src/screens/settings/index.tsx
@@ -1,21 +1,29 @@
 import React, {useState} from 'react';
 import {
+  Box,
   Center,
+  CheckIcon,
   HStack,
+  Select,
   Switch,
   Text,
   useColorMode,
   useColorModeValue,
 } from 'native-base';
+import {setLanguage} from '../../modules/settings/actions';
+import {useDispatch, useSelector} from 'react-redux';
+import {languageSelector} from '../../modules/settings/selectors';
 
 export const Settings = () => {
+  const lan = useSelector(languageSelector);
+  let [language] = useState(lan);
   const {toggleColorMode, colorMode} = useColorMode();
   const [isEnabled, setIsEnabled] = useState(colorMode === 'light');
   const toggleSwitch = () => {
     toggleColorMode();
     setIsEnabled(previousState => !previousState);
   };
-
+  const dispatch = useDispatch();
   return (
     <Center flex={1} bg={useColorModeValue('muted.100', 'blueGray.900')}>
       <HStack alignItems="center" space={4}>
@@ -27,6 +35,24 @@ export const Settings = () => {
           colorScheme="lime"
         />
       </HStack>
+      <Box maxW="300">
+        <Select
+          selectedValue={language}
+          minWidth="100"
+          accessibilityLabel="Choose language"
+          placeholder={language}
+          _selectedItem={{
+            bg: 'teal.600',
+            endIcon: <CheckIcon size="5" />,
+          }}
+          mt={1}
+          onValueChange={itemValue => {
+            dispatch(setLanguage(itemValue));
+          }}>
+          <Select.Item label="English" value="en" />
+          <Select.Item label="Spanish" value="es" />
+        </Select>
+      </Box>
     </Center>
   );
 };
