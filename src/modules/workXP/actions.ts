76965835995407f483fdd23e111bd080ec30d6e6
Warren de Leon <hi@warrendeleon.com>
2022-03-04 18:14:29 +0000

    🌐 Translations for workXP.

--- src/modules/workXP/actions.ts
+++ src/modules/workXP/actions.ts
@@ -2,12 +2,13 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 
 import {getWorkXPService} from './api';
 import {WorkXP} from '../../models/workXP';
+import {RootState} from '../../redux/configureStore';
 
-export const getWorkXP = createAsyncThunk<WorkXP[]>(
+export const getWorkXP = createAsyncThunk<WorkXP[], void, {state: RootState}>(
   'workXP/getWorkXP',
-  async (_, {rejectWithValue}) => {
+  async (_, {rejectWithValue, getState}) => {
     try {
-      const workXP = await getWorkXPService();
+      const workXP = await getWorkXPService(getState);
       return workXP.data;
     } catch (error) {
       return rejectWithValue('API Error');
--- src/modules/workXP/api.ts
+++ src/modules/workXP/api.ts
@@ -2,7 +2,12 @@ import {AxiosResponse} from 'axios';
 
 import {apiClient} from '../../httpClient';
 import {WorkXP} from '../../models/workXP';
+import {RootState} from '../../redux/configureStore';
 
-export const getWorkXPService = async (): Promise<AxiosResponse<WorkXP[]>> => {
-  return apiClient.get('workxp.json');
+export const getWorkXPService = async (
+  getState: () => RootState,
+): Promise<AxiosResponse<WorkXP[]>> => {
+  const {language} = getState().settings;
+
+  return apiClient.get(`/${language}/workxp.json`);
 };
