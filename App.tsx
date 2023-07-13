91733401010e822b03ded3025d70e6e037a650f3
Warren de Leon <hi@warrendeleon.com>
2023-07-13 01:00:53 +0100

    ✨ Added toggle to show/hide storybook on dev menu
    When showing the dev menu, there will be a new option to show/hide storybook

--- App.tsx
+++ App.tsx
@@ -1,2 +1,27 @@
+import React from 'react';
 import StorybookUIRoot from './.ondevice/Storybook';
-export {StorybookUIRoot as default};
+import {Text} from 'react-native';
+import {useCallback, useEffect, useState} from 'react';
+
+const App = (): JSX.Element => {
+  return <Text>Hello World!</Text>;
+};
+
+const Root = (): JSX.Element => {
+  const [storybookActive, setStorybookActive] = useState(false);
+  const toggleStorybook = useCallback(
+    () => setStorybookActive(active => !active),
+    [],
+  );
+
+  useEffect(() => {
+    if (__DEV__) {
+      const DevMenu = require('react-native-dev-menu');
+      DevMenu.addItem('Toggle Storybook', toggleStorybook);
+    }
+  }, [toggleStorybook]);
+
+  return storybookActive ? <StorybookUIRoot /> : <App />;
+};
+
+export default Root;
