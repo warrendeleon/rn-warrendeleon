253a90cc983bda502724013f46a672b2fda42617
Warren de Leon <hi@warrendeleon.com>
2023-07-13 20:35:32 +0100

    ♻️ Changed directory names to standarize naming.

--- src/navigators/Root.tsx
+++ src/navigators/Root.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Home from '../screens/Home/Home';
+import Home from '../screens/home/Home';
 
 const RootNavigator = (): JSX.Element => {
   const RootStack = createNativeStackNavigator();
