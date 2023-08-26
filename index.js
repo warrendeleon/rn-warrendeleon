3069b7ff7c8937eca4e6e11a1169e418693de935
Warren de Leon <hi@warrendeleon.com>
2023-08-26 18:50:00 +0100

    🐛 Temporary fix for iOS when navigation.goBack() due to a bug in Reanimated 3.4.
    Fix will come in Reanimated 3.5 (https://github.com/software-mansion/react-native-reanimated/issues/4836)

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ import {name as appName} from './app.json';
 
 import './src/i18n/i18n.config';
 import 'react-native-gesture-handler';
+import 'react-native-reanimated';
 
 AppRegistry.registerComponent(appName, () => App);
