a7d7ce63e6432b6db7b1118fa76bea6274219e11
Warren de Leon <hi@warrendeleon.com>
2025-11-04 00:32:16 +0000

    ✅ test: fix App test to work with TypeScript and Jest

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
+import App from '../App.tsx';
 
 test('renders correctly', async () => {
   await ReactTestRenderer.act(() => {
